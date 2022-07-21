e09f90a20e264234565a105cc802fc394ffb0a76
ar655 <106866721+ar655@users.noreply.github.com>
2022-07-21 16:25:47 +0300

    Update game.py

--- game.py
+++ game.py
@@ -8,15 +8,11 @@ from config import  bot,ADMIN
 
 # @dp.message_handler()
 async def echo(message:types.Message):
-   print(message)
+   
 
-   if message.from_user.id not in ADMIN:
-       await message.answer('error')
-   elif message.text.startswith('game'):
-       await bot.send_dice(message.chat.id, emoji='🏀')
+   
 
-
-   elif message.text.isnumeric():
+   if message.text.isnumeric():
 
        message.text = int(message.text)
        await bot.send_message(message.chat.id, message.text **2)
