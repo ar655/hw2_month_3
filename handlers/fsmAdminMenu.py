f02c8dfb826311d5d5a4723f05c4a67698b6eb5f
ar655 <106866721+ar655@users.noreply.github.com>
2022-07-26 11:13:48 +0300

    Update fsmAdminMenu.py

--- handlers/fsmAdminMenu.py
+++ handlers/fsmAdminMenu.py
@@ -2,6 +2,14 @@ from aiogram import types ,Dispatcher
 from aiogram.dispatcher import FSMContext
 from config import bot,ADMIN
 from aiogram.dispatcher.filters.state import State,StatesGroup
+from database import  bot_db
+from aiogram.types import  InlineKeyboardMarkup,InlineKeyboardButton
+
+
+
+
+
+
 
 
 class FSMAdmin(StatesGroup):
@@ -52,7 +60,29 @@ async  def load_price(message:types.Message,state:FSMContext):
 
 
 
+async def delete_data(message:types.Message):
+    if message.from_user.id in ADMIN and message.chat.type == 'private':
+        foods = await bot_db.sql_command_all()
+        for food in foods:
+            await bot.send_photo(message.from_user.id, food[2],
+                                 caption=f"food name:{food[3]}\n"
+                                         f"food discription:{food[4]}\n"
+                                         f"food price:{food[5]}\n",
+                                 reply_markup=InlineKeyboardMarkup().add(
+                                     InlineKeyboardButton(
+                                         f"delete: {food[3]}",
+                                         callback_data=f"delete {food[3]}"
 
+                                     )
+                                 ))
+    else:
+        await message.reply('user id error')
+
+
+async  def full_delete(call:types.CallbackQuery):
+    await bot_db.sql_command_delete(call.data.replace('delete',''))
+    await call.answer(text='food has been deleted',show_alert=True)
+    await bot.delete_message(call.message.chat.id,call.message.message_id)
 
 def register_handlers_fsmAdminmenu(dp:Dispatcher):
     dp.register_message_handler(fsm_start,commands=['update_menu'])
@@ -60,9 +90,8 @@ def register_handlers_fsmAdminmenu(dp:Dispatcher):
     dp.register_message_handler(load_name,state=FSMAdmin.name)
     dp.register_message_handler(load_dicript,state=FSMAdmin.dicript)
     dp.register_message_handler(load_price,state=FSMAdmin.price)
-
-
-
+    dp.register_message_handler(delete_data,commands=['del'])
+    dp.register_callback_query_handler(full_delete,lambda call:call.data and call.data.startswith('delete'))
 
 
 
