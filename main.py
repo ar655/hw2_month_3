0a90d8e55773670676d853f678158e342146a30c
ar655 <106866721+ar655@users.noreply.github.com>
2022-07-26 12:09:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from aiogram.utils import executor
 from config import dp
 
 import logging
-
-from handlers import  callback ,client , message,admin,fsmAdminMenu
+import  asyncio
+from handlers import  callback ,client , message,admin,fsmAdminMenu,notification
 from database.bot_db import sql_create
 
 fsmAdminMenu.register_handlers_fsmAdminmenu(dp)
+notification.register_func(dp)
 client.register_handler_q(dp)
 callback.register_handlers_query(dp)
 admin.register_handlers_extra(dp)
@@ -17,6 +18,7 @@ message.register_handler_some(dp)
 
 
 async def on_startup(_)
+    asyncio.create_task(notification.scheduler())
     sql_create()
 
 
