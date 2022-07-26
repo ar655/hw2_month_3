10794422e0bced2fa3923859ec91cd76160c7e6d
ar655 <106866721+ar655@users.noreply.github.com>
2022-07-26 11:22:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from config import dp
 import logging
 
 from handlers import  callback ,client , message,admin,fsmAdminMenu
-
+from database.bot_db import sql_create
 
 fsmAdminMenu.register_handlers_fsmAdminmenu(dp)
 client.register_handler_q(dp)
@@ -16,8 +16,12 @@ message.register_handler_some(dp)
 
 
 
+async def on_startup(_)
+    sql_create()
+
+
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp,skip_updates=True)
+    executor.start_polling(dp,skip_updates=True,on_startup=on_startup)
